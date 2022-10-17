4f9e781d55845d480259cd9f422b9593df310597
Dima Honcharenko <d.goncharenko.man@gmail.com>
2022-10-17 10:53:53 +0300

    Adjust Postgre connection

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -10,9 +10,22 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: +process.env.DB_PORT!,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+
+  username:
+    process.env.NODE_ENV === "production"
+      ? process.env.PRODUCTION_DB_USER
+      : process.env.DB_USER,
+
+  password:
+    process.env.NODE_ENV === "production"
+      ? process.env.PRODUCTION_DB_PASS
+      : process.env.DB_PASS,
+
+  database:
+    process.env.NODE_ENV === "production"
+      ? process.env.PRODUCTION_DB_NAME
+      : process.env.DB_NAME,
+
   synchronize: true,
   logging: true,
   entities: [Client, Project],
